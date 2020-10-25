837168cf9767993ac397eae0483e8e9815b428b1
PhongpisutMM <69627422+PhongpisutMM@users.noreply.github.com>
2020-10-25 19:56:29 +0700

    1.1.0

--- src/Databoard.js
+++ src/Databoard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import { Pagination ,TransitionablePortal, Button, Segment, Header,Input} from 'semantic-ui-react'
+import { Pagination ,TransitionablePortal, Button, Segment, Header,Input,Dropdown,Grid,Confirm} from 'semantic-ui-react'
 
 
 class Databoard extends React.Component {
@@ -17,7 +17,12 @@ class Databoard extends React.Component {
                     "end":0
                 },
                 portalopen:false,
-                text:"ข้อความ"
+                text:"ข้อความ",
+                seltag:"Location",
+                error:{
+                    status:false,
+                    msg:""
+                }
 
             }
         
@@ -27,6 +32,10 @@ class Databoard extends React.Component {
             this.textareahandle = this.textareahandle.bind(this);
             this.Updatebg = this.Updatebg.bind(this);
             this.checktext = this.checktext.bind(this);
+            this.tag_select = this.tag_select.bind(this);
+            this.doPost = this.doPost.bind(this);
+            this.confirm = this.confirm.bind(this);
+
 
          
         }
@@ -67,8 +76,41 @@ class Databoard extends React.Component {
 
         }
 
+        confirm(i){
+            this.setState({error:{status:false,msg:""}})
+            if(i===0){}
+            if(i===1){}
+
+        }
+
+
         handleOpen = () => this.setState({ portalopen: true ,pick:{begin:0,end:0},text:"ข้อความ" })
         handleClose = () => this.setState({ portalopen: false })
+
+
+        doPost(data){
+
+            if((this.state.pick.begin===0&&this.state.pick.end===0 )|| this.state.pick.begin===this.state.pick.end){
+                this.setState({error:{status:true,msg:"Please Select Text"}});
+            }
+
+            var have =0
+
+            data.mentions.map(mention=>{
+                
+                if(mention.location.begin === this.state.pick.begin && mention.location.end === this.state.pick.end)
+                    {   have+=1
+                        this.setState({error:{status:true,msg:"This Tag already exists!"}});
+                    }
+                    return[]
+                })
+
+                if(have===0){
+                    console.log("PUT SO NICE") }
+    
+            
+        
+        }
 
 
         Updatestate(data,i){
@@ -100,13 +142,17 @@ class Databoard extends React.Component {
         }
     
 
-
+        tag_select(data){
+            this.setState({seltag:data.value})
+        }
         checktext(){
             if(this.state.text===""){
                 return ""
             }
             return this.state.text
         }
+        
+
 
 
 
@@ -160,14 +206,45 @@ class Databoard extends React.Component {
 
             const { portalopen } = this.state.portalopen
 
-       
-
+            const Tags = 
+            [
+                { key: 'A', value: 'Address', text: 'Address' },
+                { key: 'Da', value: 'Date', text: 'Date' },
+                { key: 'Du', value: 'Duration', text: 'Duration' },
+                { key: 'E', value: 'EmailAddress', text: 'EmailAddress' },
+                { key: 'F', value: 'Facility', text: 'Facility' },
+                { key: 'G', value: 'GeographicFeature', text: 'GeographicFeature' },
+                { key: 'H', value: 'Hashtag', text: 'Hashtag' },
+                { key: 'J', value: 'JobTitle', text: 'JobTitle' },
+                { key: 'L', value: 'Location', text: 'Location' },
+                { key: 'I', value: 'IPAddress', text: 'IPAddress' },
+                { key: 'Me', value: 'Measure', text: 'Measure' },
+                { key: 'Mo', value: 'Money', text: 'Money' },
+                { key: 'N', value: 'Number', text: 'Number' },
+                { key: 'Ord', value: 'Ordinal', text: 'Ordinal' },
+                { key: 'Org', value: 'Organization', text: 'Organization' },
+                { key: 'Pers', value: 'Person', text: 'Person' },
+                { key: 'Perc', value: 'Percent', text: 'Percent' },
+                { key: 'Pro', value: 'Product', text: 'Product' },
+                { key: 'Pho', value: 'PhoneNumber', text: 'PhoneNumber' },
+                { key: 'Tw', value: 'TwitterHandle', text: 'TwitterHandle' },
+                { key: 'Ti', value: 'Time', text: 'Time' },
+                { key: 'URL', value: 'URL', text: 'URL' },
+                
+               
+              ]
         // Get Data Total
        
 
 
                 return (  
                 <div> 
+                    <Confirm
+          open={this.state.error.status}
+          content={this.state.error.msg}
+          onCancel={()=>this.confirm(0)}
+          onConfirm={()=>this.confirm(1)}
+        />
                     { currentpost.map((data ,i) => { 
                       return [
                     <div key={"a" + data.id} > 
@@ -199,15 +276,40 @@ class Databoard extends React.Component {
                     />}>
                         
                     <Segment
-                    style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000 }}
+                    style={{ left: '30%', position: 'fixed', top: '20%', zIndex: 1000 ,width:'40%',height:'67%'}}
                     >
                     <Header>ข้อความรหัสที่ {data.id}</Header>
-                    <p onMouseUp={this.textareahandle}>{data.text}</p>
+                    <Grid columns={2} divided style={{marginRight:'5px',marginLeft:'5px'}}>
+                    <Grid.Row>
+                        <Segment style={{width:"90%"}}>  <p onMouseUp={this.textareahandle}>{data.text}</p> </Segment>
+                   
+                    </Grid.Row>
+                    <Grid.Row>
+                   <Grid.Column>
+                <Input placeholder='ข้อความ' value={this.state.text} style={{width:"100%"}}  /> 
+                 
+                   </Grid.Column>
+                   
+                   <Grid.Column>
+                      <Button disabled={true} >{this.state.pick.begin}</Button>
+                    <Button disabled={true} >{this.state.pick.end}</Button>
+                   
+                </Grid.Column>
                     
-                    <button className ="ui primary button"> POST</button>
-                    <Input placeholder='ข้อความ' value={this.state.text} style={{width:"45%", "marginRight":"5px"}}  />
-                    <button className ="ui  button" disabled={true} >{this.state.pick.begin}</button>
-                    <button className ="ui  button" disabled={true} >{this.state.pick.end}</button>
+            
+                    
+                    </Grid.Row>
+                  
+                    
+                    <Grid.Row> <Dropdown placeholder='State' fluid search selection options={Tags} style={{MarginTop:'10%'}} onChange={(e,data)=>this.tag_select(data)}/></Grid.Row>
+                    <Grid.Row>
+                    
+                     <Button className ="ui primary button" onClick={()=>this.doPost(data)} > POST</Button> 
+                    <Button className ="ui yellow button"> PUT</Button>
+                    <Button className ="ui red button" > DELETE</Button>
+                    </Grid.Row>
+                    
+                    </Grid>
                     </Segment>
                     </TransitionablePortal>
 
